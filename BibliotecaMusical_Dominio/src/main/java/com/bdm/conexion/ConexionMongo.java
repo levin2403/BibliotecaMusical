59e27bb2a6610b14e9fb3d4b0da96c675c0275c9
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2024-11-24 00:10:56 -0700

    Cambios en negocio
    Agregacion de paquetes y clases convertidoras

---
+++ BibliotecaMusical_Dominio/src/main/java/com/bdm/conexion/ConexionMongo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bdm.conexion;
+
+import com.bmd.conexionIntefaces.IConexionMongo;
+
+/**
+ *
+ * @author skevi
+ */
+public class ConexionMongo implements IConexionMongo {
+    
+}
