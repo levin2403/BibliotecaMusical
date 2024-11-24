d243b534cf3ee4fce49eb8a8f730f6137d0ddf2c
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2024-11-24 03:05:15 -0700

    asdasasd
    asdasd

--- BibliotecaMusical_Dominio/src/main/java/com/bmd/entities/Artista.java
+++ BibliotecaMusical_Dominio/src/main/java/com/bmd/entities/Artista.java
@@ -5,6 +5,7 @@
 package com.bmd.entities;
 
 import com.bmd.enums.Genero;
+import com.bmd.enums.TipoArtista;
 
 /**
  *
@@ -13,22 +14,31 @@ import com.bmd.enums.Genero;
 import java.util.List;
 
 public class Artista {
+    
     private final String id;
+    private final TipoArtista tipoArtista;
     private final String nombre;
     private final String imagen;
     private final Genero genero;
+    private final List<Integrante> integrantes;
     private final List<Album> albums;
 
     private Artista(Builder builder) {
         this.id = builder.id;
+        this.tipoArtista = builder.tipoArtista;
         this.nombre = builder.nombre;
         this.imagen = builder.imagen;
         this.genero = builder.genero;
+        this.integrantes = builder.integrantes;
         this.albums = builder.albums;
     }
 
     public String getId() {
         return id;
+    }
+
+    public TipoArtista getTipoArtista() {
+        return tipoArtista;
     }
 
     public String getNombre() {
@@ -43,26 +53,32 @@ public class Artista {
         return genero;
     }
 
+    public List<Integrante> getIntegrantes() {
+        return integrantes;
+    }
+
     public List<Album> getAlbums() {
         return albums;
     }
 
     @Override
     public String toString() {
-        return "Artista{" +
-               "id='" + id + '\'' +
-               ", nombre='" + nombre + '\'' +
-               ", imagen='" + imagen + '\'' +
-               ", genero=" + genero +
-               ", albums=" + albums +
-               '}';
+        return "id: " + id + 
+                ", tipoArtista: " + tipoArtista + 
+                ", nombre: " + nombre + 
+                ", imagen: " + imagen + 
+                ", genero: " + genero + 
+                ", integrantes: " + integrantes + 
+                ", albums: " + albums + '}';
     }
     
     public static class Builder {
         private String id;
+        private TipoArtista tipoArtista;
         private String nombre;
         private String imagen;
         private Genero genero;
+        private List<Integrante> integrantes;
         private List<Album> albums;
 
         public Builder setId(String id) {
@@ -70,6 +86,11 @@ public class Artista {
             return this;
         }
 
+        public Builder setTipoArtista(TipoArtista tipoArtista){
+            this.tipoArtista = tipoArtista;
+            return this;
+        }
+        
         public Builder setNombre(String nombre) {
             this.nombre = nombre;
             return this;
@@ -82,6 +103,11 @@ public class Artista {
 
         public Builder setGenero(Genero genero) {
             this.genero = genero;
+            return this;
+        }
+        
+        public Builder setIntegrantes(List<Integrante> integrantes) {
+            this.integrantes = integrantes;
             return this;
         }
 
