750973f69600f960b1fdde4f52a8dfcbf6ed808c
EduardoCastelo26 <link6698@gmail.com>
2024-12-03 11:13:38 -0600

    Correccion insercion masiva

--- BibliotecaMusical_Negocio/src/main/java/com/bmn/negocio/NegocioArtistas.java
+++ BibliotecaMusical_Negocio/src/main/java/com/bmn/negocio/NegocioArtistas.java
@@ -5,7 +5,6 @@ import com.bmd.entities.Album;
 import com.bdm.conexion.ConexionMongo;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
-import org.bson.types.ObjectId;
 
 import java.util.List;
 
@@ -21,40 +20,53 @@ public class NegocioArtistas {
     }
 
     public void insertarArtistasYAlbums(List<Artista> artistas, List<Album> albums) {
+        // Imprimir el tamaño de las listas
+        System.out.println("Número de artistas: " + artistas.size());
+        System.out.println("Número de álbumes: " + albums.size());
+
+        // Imprimir los nombres de los artistas
+        for (Artista artista : artistas) {
+            System.out.println("Artista: " + artista.getNombre());
+        }
+
+        // Imprimir los nombres de los álbumes
+        for (Album album : albums) {
+            System.out.println("Álbum: " + album.getNombre());
+        }
+
         // Inserta los artistas en MongoDB
         for (Artista artista : artistas) {
             Document document = new Document()
-                .append("_id", new ObjectId()) // Usar ObjectId para los IDs
+                .append("_id", artista.getId()) // Utilizar el ObjectId generado
                 .append("tipoArtista", artista.getTipoArtista())
                 .append("nombre", artista.getNombre())
                 .append("imagen", artista.getImagen())
                 .append("genero", artista.getGenero());
             collectionArtista.insertOne(document);
+            System.out.println("Artista insertado: " + artista.getNombre());
         }
 
         // Inserta los álbumes en MongoDB
-        // Inserta los álbumes en MongoDB
-for (Album album : albums) {
-    try {
-        Document document = new Document()
-            .append("_id", new ObjectId()) // Usar ObjectId para los IDs
-            .append("nombre", album.getNombre())
-            .append("imagenPortada", album.getImagenPortada())
-            .append("fechaLanzamiento", album.getFechaLanzamiento())
-            .append("genero", album.getGenero())
-            .append("artista", new Document("id", new ObjectId())
-                                 .append("tipoArtista", album.getArtista().getTipoArtista())
-                                 .append("nombre", album.getArtista().getNombre())
-                                 .append("imagen", album.getArtista().getImagen())
-                                 .append("genero", album.getArtista().getGenero()))
-            .append("canciones", album.getCanciones());
-        collectionAlbum.insertOne(document);
-        System.out.println("Álbum insertado: " + album.getNombre());
-    } catch (Exception e) {
-        System.err.println("Error al insertar álbum: " + album.getNombre() + " - " + e.getMessage());
-    }
-}
-
+        for (Album album : albums) {
+            try {
+                Document document = new Document()
+                    .append("_id", album.getId()) // Utilizar el ObjectId generado
+                    .append("nombre", album.getNombre())
+                    .append("imagenPortada", album.getImagenPortada())
+                    .append("fechaLanzamiento", album.getFechaLanzamiento())
+                    .append("genero", album.getGenero())
+                    .append("artista", new Document("_id", album.getArtista().getId()) // Utilizar el ObjectId del artista generado
+                                         .append("tipoArtista", album.getArtista().getTipoArtista())
+                                         .append("nombre", album.getArtista().getNombre())
+                                         .append("imagen", album.getArtista().getImagen())
+                                         .append("genero", album.getArtista().getGenero()))
+                    .append("canciones", album.getCanciones());
+                collectionAlbum.insertOne(document);
+                System.out.println("Álbum insertado: " + album.getNombre());
+            } catch (Exception e) {
+                System.err.println("Error al insertar álbum: " + album.getNombre() + " - " + e.getMessage());
+            }
+        }
 
         System.out.println("Inserción de artistas solistas y álbumes completada exitosamente");
     }
