6b14cfff1335e124c4b39fa15691e3ad8302f3c2
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2024-12-04 21:00:52 -0700

    asdasdasd
    asdasd

--- BibliotecaMusical_Negocio/src/main/java/com/bmn/pruebas/PruebaObtenerAlbumesFiltrados.java
+++ BibliotecaMusical_Negocio/src/main/java/com/bmn/pruebas/PruebaObtenerAlbumesFiltrados.java
@@ -4,6 +4,12 @@
  */
 package com.bmn.pruebas;
 
+import com.bmn.dto.AlbumVistaDTO;
+import com.bmn.excepciones.BOException;
+import com.bmn.factories.BOFactory;
+import com.bmn.negocio.ObtenerAlbumesFiltradosBO;
+import java.util.List;
+
 /**
  *
  * @author skevi
@@ -13,8 +19,16 @@ public class PruebaObtenerAlbumesFiltrados {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws BOException {
+        
+        ObtenerAlbumesFiltradosBO obtener = BOFactory.obtenerAlbumesFiltradosFactory();
+        
+        List<AlbumVistaDTO> albumes = obtener.BuscarPorFiltro("cumbia", null, null);
+        
+        for (AlbumVistaDTO album : albumes) {
+            System.out.println(album.toString());
+        }
+        
     }
     
 }
